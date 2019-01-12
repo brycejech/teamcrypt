c9f6632ca32b949e29ea5e13871bc72fe2eba53b
Bryce Jech <bryce@brycejech.com>
2019-01-12 17:15:22 -0600

    add keyfile encryption methods

--- models/Keyfile.js
+++ models/Keyfile.js
@@ -1,15 +1,18 @@
 'use strict';
 
+const crypto = require('../app/crypto');
+
 function Keyfile(userID, file=[]){
-    this.userID  = userID;
-    this.keyfile = file;
+    this.userID    = userID;
+    this.keyfile   = file;
+    this.encrypted = typeof file === 'string'
 
     return this;
 }
 
 Keyfile.prototype.add = function keyfileAdd(obj){
-
-    if(!obj.title) throw new Error('Must provide a title')
+    if(this.encrypted) throw new Error('Must decrypt before adding items');
+    if(!obj.title)     throw new Error('Must provide a title')
 
     let { title, type, parent, username, password, url, notes } = obj;
 
@@ -55,6 +58,33 @@ Keyfile.prototype.remove = function keyfileRemove(title){
 
 Keyfile.prototype.findEntry = function keyfileFindEntry(title){
     return this.keyfile.filter(entry => entry.title === title)[0];
+}
+
+Keyfile.prototype.decrypt = function keyfileDecrypt(key){
+
+    if(!this.encrypted) return this;
+
+    const decrypted = crypto.decrypt(key, this.keyfile);
+
+    if(decrypted){
+        this.keyfile   = decrypted;
+        this.encrypted = false;
+
+        return this;
+    }
+    else{
+        throw new Error('Incorrect encryption key');
+    }
+}
+
+Keyfile.prototype.encrypt = function keyfileEncrypt(key){
+    try{
+        this.keyfile   = crypto.encrypt(key, this.keyfile);
+        this.encrypted = true;
+
+        return this;
+    }
+    catch(e){ throw e }
 }
 
 
