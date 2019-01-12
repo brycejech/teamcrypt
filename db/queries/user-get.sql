6dc9105086658f6d495a14a0587029c18c90b65c
Bryce Jech <bryce@brycejech.com>
2019-01-12 16:46:03 -0600

    add user-get query

---
+++ db/queries/user-get.sql
@@ -0,0 +1,22 @@
+
+SELECT
+    account.id,
+    account.name,
+    account.email,
+    account.username,
+    account.password,
+    account.uuid,
+    account.registered,
+    
+    keyfile.keyfile,
+    keyfile.id          AS "keyfileID",
+    keyfile.modified    AS "keyfileLastModified"
+
+FROM account
+
+LEFT OUTER JOIN keyfile on keyfile.account = account.id
+
+WHERE
+       account.username = $1
+    OR account.email    = $1
+;
