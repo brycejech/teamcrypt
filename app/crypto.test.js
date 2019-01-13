42fd4616635c9bce49f752afe28f7d6bb2fb9229
Bryce Jech <bryce@brycejech.com>
2019-01-13 11:04:39 -0600

    unit tests for crypto.verifyPassword

--- app/crypto.test.js
+++ app/crypto.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { encrypt, decrypt, deriveKey } = require('./crypto');
+const { encrypt, decrypt, deriveKey, hashPassword, verifyPassword } = require('./crypto');
 
 const password = 'superSecr3tPassphraze',
       salt     = 'dk2lkj2090k3-s92ks,s0';
@@ -45,6 +45,40 @@ describe('Crypto module should encrypt text', async () => {
 
         expect(decrypt(key2, encrypted)).toBe(null);
 
+    });
+
+});
+
+describe('Crypto module should hash and verify passwords', () => {
+
+    let pass1, hash1, pass2, hash2;
+
+    beforeEach(async () => {
+        pass1 = 'mySecureP4$$wor!d';
+        hash1 = await hashPassword(pass1);
+        pass2 = 'myAlt3rn4tIvePassw0r^d';
+        hash2 = await hashPassword(pass2);
+    });
+
+    test('It should hash a password', async () => {
+        expect(typeof hash1).toBe('string');
+        expect(hash1 === pass1).toBe(false);
+        expect(hash1.length).toBe(60);
+    });
+
+    test('It should verify a hashed password', async () => {
+        expect(await verifyPassword(pass1, hash1)).toBe(true);
+    });
+
+    test('It should not verify incorrect passwords', async () => {
+        expect(await verifyPassword(pass2, hash1)).toBe(false);
+        expect(await verifyPassword(pass1, hash2)).toBe(false);
+    });
+
+    test('It should reject on undefined values', () => {
+        expect.assertions(1);
+
+        return expect(hashPassword(undefined)).rejects.toEqual(new Error('data and salt arguments required'));
     });
 
 });
