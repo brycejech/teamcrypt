7587692de71abc8dec9cbed48befc1fe2321e9b6
Bryce Jech <bryce@brycejech.com>
2019-01-13 12:16:20 -0600

    add a login route

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const express    = require('express'),
       bodyParser = require('body-parser'),
       path       = require('path');
 
+const app = require('./app'),
+      db  = require('./db');
+
+const { Keyfile } = require('./models');
+
 const server = express();
 
 server.set('case sensitive routing', true);
@@ -24,6 +29,29 @@ server.use((req, res, next) => {
 
 server.get('/', (req, res, next) => {
     return res.json({ message: 'Running' });
+});
+
+server.post('/login', async (req, res, next) => {
+    const username = req.body.username || req.body.email,
+          password = req.body.password;
+
+    const user   = await db.q('user-get', [username]),
+          authed = await app.crypto.verifyPassword(password, user.password);
+
+    if(authed === true){
+        const kf = new Keyfile(user.id, user.keyfile);
+
+        const { key } = await app.crypto.deriveKey(password, user.salt);
+
+        user.keyfile = kf.decrypt(key);
+
+        const publicUser = app.makePublic.user(user);
+
+        res.json(publicUser);
+    }
+    else{
+        res.json({ message: 'Incorrect username/password' });
+    }
 });
 
 server.listen(8080);
