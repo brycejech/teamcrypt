3cc60abe2a129c52b1e743e2d560c5323f873938
Bryce Jech <bryce@brycejech.com>
2019-01-21 20:45:27 -0600

    add sass to project

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,37 @@
 'use strict';
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 const path = require('path');
 
-module.exports = {
-    entry: './client/src/js/index.js',
+const webpackConfig = {
     output: {
         path: path.resolve(__dirname, './client/dist'),
-        filename: 'bundle.min.js'
+        filename: '[name].min.js'
     },
     mode: 'production',
     devtool: 'source-map'
 }
+
+const jsConfig = Object.assign({}, webpackConfig, {
+    entry: [ './client/src/js/index.js' ]
+});
+
+const sassConfig = Object.assign({}, webpackConfig, {
+    entry: [ './client/src/sass/main.sass' ],
+    module: {
+        rules: [
+            {
+                test: /\.sass$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].bundle.css'
+        })
+    ]
+});
+
+module.exports = [ jsConfig, sassConfig ];
