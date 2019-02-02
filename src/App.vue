0c617b75da3664bba49bc77a824294958e2536f0
Bryce Jech <bryce@brycejech.com>
2019-02-02 11:58:28 -0600

    Main app file

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-link to="/login">Login</router-link>
+    <router-link to="/register">Register</router-link>
+    <router-link to="/add-entry">Add Entry</router-link>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="sass">
+#app
+    font-family: 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing: antialiased
+    -moz-osx-font-smoothing: grayscale
+    text-align: center
+    color: #2c3e50
+    margin-top: 60px
+
+[class*="col-"] .row
+    margin-left: -1rem
+    margin-right: -1rem
+</style>
