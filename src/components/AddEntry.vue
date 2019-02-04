2308302ce71598b2e976c424d852e3ff50504d59
Bryce Jech <bryce@brycejech.com>
2019-02-03 21:29:39 -0600

    remove form padding

--- src/components/AddEntry.vue
+++ src/components/AddEntry.vue
@@ -1,45 +1,45 @@
 <template lang="html">
     <div class="add-entry vue">
         <form @submit.prevent="formSubmit">
-            <h3 class="text-center">Vue Add Entry</h3>
+            <h2>Add Entry</h2>
 
             <div class="row">
-                <div class="col-12">
+                <div class="col-12 pb-0">
                     <label for="v-type">Type</label>
                     <select id="v-type" v-model="type">
                         <option value="entry" selected>Entry</option>
                         <option value="folder">Folder</option>
                     </select>
                 </div>
-                <div class="col-12">
+                <div class="col-12 pb-0">
                     <label for="v-title">Title</label>
                     <input id="v-title" v-model="title" type="text">
                 </div>
-                <div class="col-12">
+                <div class="col-12 pb-0">
                     <label for="v-parent">Parent</label>
                     <input id="v-parent" v-model="parent" type="text">
                 </div>
-                <div v-if="type === 'entry'" class="col-12">
+                <div v-if="type === 'entry'" class="col-12 pb-0">
                     <label for="v-username">Username</label>
                     <input id="v-username" v-model="username" type="text">
                 </div>
-                <div v-if="type === 'entry'" class="col-6">
+                <div v-if="type === 'entry'" class="col-6 pb-0">
                     <label for="v-password">Password</label>
                     <input id="v-password" v-model="password" type="password">
                 </div>
-                <div v-if="type === 'entry'" class="col-6">
+                <div v-if="type === 'entry'" class="col-6 pb-0">
                     <label for="v-confirm-password">Confirm Password</label>
                     <input id="v-confirm-password" v-model="confirmPassword" type="password">
                 </div>
-                <div v-if="type === 'entry'" class="col-12">
+                <div v-if="type === 'entry'" class="col-12 pb-0">
                     <label for="v-url">URL</label>
                     <input id="v-url" v-model="url" type="text">
                 </div>
-                <div v-if="type === 'entry'" class="col-12">
+                <div v-if="type === 'entry'" class="col-12 pb-0">
                     <label for="v-notes">Notes</label>
                     <textarea id="v-notes" v-model="notes" rows="6"></textarea>
                 </div>
-                <div class="col-6 col-offset-3">
+                <div class="col-6 col-offset-3 pb-0">
                     <button class="submit btn btn-block btn-primary">Submit</button>
                 </div>
             </div>
