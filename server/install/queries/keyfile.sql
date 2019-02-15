2b14c535c5022f103c1becb849b19e5f1b93717f
Bryce Jech <bryce@brycejech.com>
2019-02-15 07:25:39 -0600

    add salt to keyfile table

--- server/install/queries/keyfile.sql
+++ server/install/queries/keyfile.sql
@@ -2,9 +2,10 @@ DROP TABLE IF EXISTS keyfile CASCADE;
 
 CREATE TABLE keyfile
 (
-    id           serial NOT NULL PRIMARY KEY,
-    account      int    NOT NULL REFERENCES account(id),
+    id           serial   NOT NULL PRIMARY KEY,
+    account      int      NOT NULL REFERENCES account(id),
     keyfile      text,
+    salt         char(64) NOT NULL,
 
     created  timestamp with time zone DEFAULT (now() at time zone 'utc'),
     modified timestamp with time zone
