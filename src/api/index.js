33db50f4cbe111d140bed4653440f8948f331664
Bryce Jech <bryce@brycejech.com>
2019-02-16 08:02:15 -0600

    use new api routes, add register method

--- src/api/index.js
+++ src/api/index.js
@@ -6,7 +6,7 @@ export default api;
 
 api.login = async function login(username, password){
 
-    const url = '/login';
+    const url = '/api/login';
     const opt = {
         method: 'POST',
         mode: 'same-origin',
@@ -26,9 +26,30 @@ api.login = async function login(username, password){
     }
 }
 
+api.register = async function register(data){
+    const url = '/api/register',
+          opt = {
+              method: 'POST',
+              mode: 'same-origin',
+              headers: {
+                  'Content-Type': 'application/json; charset=utf-8'
+              },
+              body: JSON.stringify(data)
+          }
+    try{
+
+        const r = await fetch(url, opt);
+
+        return r.json()
+    }
+    catch(e){
+        console.error(e);
+    }
+}
+
 api.getUsers = async function getUsers(){
 
-    const url = '/users';
+    const url = '/api/users';
     const opt = {
         method: 'GET',
         mode: 'same-origin'
@@ -46,7 +67,7 @@ api.getUsers = async function getUsers(){
 
 api.updateKeyfile = async function updateRemoteKeyfile(keyfile){
 
-    const url = '/user/keyfile',
+    const url = '/api/user/keyfile',
           opt = {
               method: 'POST',
               mode: 'same-origin',
