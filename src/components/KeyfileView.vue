899a6777452ec2558eb743e8636fb1ca6fa8f41e
Bryce Jech <bryce@brycejech.com>
2019-02-16 17:00:53 -0600

    add KeyfileView component

---
+++ src/components/KeyfileView.vue
@@ -0,0 +1,45 @@
+<template lang="html">
+    <div class="keyfile-view">
+        <h3>Entries</h3>
+        <table>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>URL</th>
+                    <th>Username</th>
+                    <th>Tags</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="entry in entries">
+                    <td>{{ entry.title }}</td>
+                    <td>{{ entry.url }}</td>
+                    <td>{{ entry.username }}</td>
+                    <td>{{ entry.tags }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+'use strict';
+
+export default {
+    name: 'KeyfileView',
+    computed: {
+        entries(){
+            return this.$store.getters.keyfileEntries;
+        }
+    },
+    created(){
+        
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+    table
+        tr:nth-child(even)
+            background-color: rgba(black, .05)
+</style>
