81d9f9b4060e91ccbee85fcf4f8743a006aa4985
Bryce Jech <bryce@brycejech.com>
2019-02-16 18:08:16 -0600

    display tags as "pills"

--- src/components/KeyfileView.vue
+++ src/components/KeyfileView.vue
@@ -11,11 +11,13 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="entry in entries">
+                <tr v-for="entry in entries" :data-id="entry.uuid">
                     <td>{{ entry.title }}</td>
                     <td>{{ entry.url }}</td>
                     <td>{{ entry.username }}</td>
-                    <td>{{ entry.tags }}</td>
+                    <td>
+                        <span class="pill" v-for="tag in entry.tags">{{ tag }}</span>
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -33,7 +35,7 @@ export default {
         }
     },
     created(){
-        
+
     }
 }
 </script>
@@ -42,4 +44,10 @@ export default {
     table
         tr:nth-child(even)
             background-color: rgba(black, .05)
+    .pill
+        display: inline-block
+        padding: 3px
+        margin-right: 3px
+        background: #3F3F3F
+        color: white
 </style>
