a3a3a03e718dc1783da9033d5fd008e97bfd992f
Bryce Jech <bryce@brycejech.com>
2019-02-16 18:08:05 -0600

    remove "type" switch, all entries are entries (no folders)

--- src/components/AddEntry.vue
+++ src/components/AddEntry.vue
@@ -4,17 +4,10 @@
             <div class="modal-content">
                 <div class="add-entry vue">
                     <form @submit.prevent="formSubmit">
-                        <h2>Add Entry</h2>
+                        <h1 class="text-center mt-2 mb-0">Add Entry</h1>
 
                         <div class="container">
                             <div class="row">
-                                <div class="col-12 pb-0">
-                                    <label for="v-type">Type</label>
-                                    <select id="v-type" v-model="type" class="mb-0">
-                                        <option value="entry" selected>Entry</option>
-                                        <option value="folder">Folder</option>
-                                    </select>
-                                </div>
                                 <div class="col-12 pb-0">
                                     <label for="v-title">Title</label>
                                     <input id="v-title" v-model="title" type="text" class="mb-0">
@@ -25,23 +18,23 @@
                                         <input @input="showTags" id="v-tag" v-model="tag" type="text" class="mb-0">
                                     </form>
                                 </div>
-                                <div v-if="type === 'entry'" class="col-12 pb-0">
+                                <div class="col-12 pb-0">
                                     <label for="v-username">Username</label>
                                     <input id="v-username" v-model="username" type="text" class="mb-0">
                                 </div>
-                                <div v-if="type === 'entry'" class="col-6 pb-0">
+                                <div class="col-6 pb-0">
                                     <label for="v-password">Password</label>
                                     <input id="v-password" v-model="password" type="password" class="mb-0">
                                 </div>
-                                <div v-if="type === 'entry'" class="col-6 pb-0">
+                                <div class="col-6 pb-0">
                                     <label for="v-confirm-password">Confirm Password</label>
                                     <input id="v-confirm-password" v-model="confirmPassword" type="password" class="mb-0">
                                 </div>
-                                <div v-if="type === 'entry'" class="col-12 pb-0">
+                                <div class="col-12 pb-0">
                                     <label for="v-url">URL</label>
                                     <input id="v-url" v-model="url" type="text" class="mb-0">
                                 </div>
-                                <div v-if="type === 'entry'" class="col-12 pb-0">
+                                <div class="col-12 pb-0">
                                     <label for="v-notes">Notes</label>
                                     <textarea id="v-notes" v-model="notes" rows="6" class="mb-0"></textarea>
                                 </div>
@@ -64,7 +57,6 @@ export default {
     name: 'AddEntry',
     data(){
         return {
-            type:            'entry',
             title:           '',
             tag:             '',
             username:        '',
@@ -82,7 +74,6 @@ export default {
     methods: {
         formSubmit(e){
             const data = {
-                type:            this.type,
                 title:           this.title,
                 tags:            this.tags,
                 username:        this.username,
@@ -98,12 +89,13 @@ export default {
         },
         reset(){
             this.title           = '';
-            this.parent          = '';
+            this.tag             = '';
             this.username        = '';
             this.password        = '';
             this.confirmPassword = '';
             this.url             = '';
             this.notes           = '';
+            this.tags            = [];
         },
         showTags(){
             console.log(this.$store.getters.keyfileTags);
