ce03dc3fa36db34892d94356c54c81acd6c48a3b
Bryce Jech <bryce@brycejech.com>
2019-02-18 06:36:11 -0600

    don't render uuid

--- src/components/KeyfileView.vue
+++ src/components/KeyfileView.vue
@@ -8,7 +8,7 @@
                     <th>URL</th>
                     <th>Username</th>
                     <th>Tags</th>
-                    <th>UUID</th>
+                    <!-- <th>UUID</th> -->
                     <th>Edit</th>
                 </tr>
             </thead>
@@ -20,7 +20,7 @@
                     <td>
                         <span class="pill" v-for="tag in entry.tags">{{ tag }}</span>
                     </td>
-                    <td>{{ entry.uuid }}</td>
+                    <!-- <td>{{ entry.uuid }}</td> -->
                     <td>
                         <keyfile-edit :entry="entry" />
                     </td>
